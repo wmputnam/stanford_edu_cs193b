b6edc54ee4fda4b0c471f9d20ff775196e3a4498
William Putnam <wmputnam@gmail.com>
2024-11-22 16:08:24 -0800

    pre-lecture Apple code

---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by William Putnam on 11/22/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
