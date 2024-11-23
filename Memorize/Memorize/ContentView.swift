db6ed0cf910ec312468937197e5c2bd85fae43c2
William Putnam <wmputnam@gmail.com>
2024-11-22 16:13:45 -0800

    lecture 1 code from QiruJin/CS193P-Swift https://github.com/QiruJin/CS193P-Swift/tree/3675866ed6925f9b4ef266bb022a833dc5451f3a

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -8,17 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    // VStack：up and down, vertical stack
+    // HStack: side to side, horizontal
+    // ZStack: direction towards the user
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, Bill2!")
+        HStack{
+            CardView(isFaceUp: true)
+            CardView()
+            CardView()
         }
+        .foregroundColor(.orange)
         .padding()
     }
 }
 
-#Preview {
-    ContentView()
+struct CardView: View{
+    var isFaceUp: Bool = false
+    
+    var body: some View {
+        ZStack(content: {
+            if isFaceUp{
+                RoundedRectangle(cornerRadius: 12)
+                    .foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 12)
+                    .strokeBorder(lineWidth: 2)
+                Text("💥").font(.largeTitle)
+            }
+            else{
+                RoundedRectangle(cornerRadius: 12)
+            }
+        })
+    }
+}
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
